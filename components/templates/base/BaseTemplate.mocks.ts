880f474d2c12dd4af62ba86d70c5f21b13939187
Manish Shrestha <18145348+manishshres@users.noreply.github.com>
2022-06-21 21:34:14 -0400

    feat: add BaseTemplate component

---
+++ components/templates/base/BaseTemplate.mocks.ts
@@ -0,0 +1,13 @@
+import { IBaseTemplate } from './BaseTemplate';
+
+const base: IBaseTemplate = {
+  sampleTextProp: 'Manish',
+};
+
+const alt: IBaseTemplate = {
+  sampleTextProp: 'Test',
+};
+
+export const mockBaseTemplateProps = {
+  base,
+};
